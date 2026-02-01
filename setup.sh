f4dd7f12f343e1ed97a87209fd2b364c6f172c9f
tedcrypto5-create <tedcrypto5@gmail.com>
2026-02-01 08:44:41 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 BASE="https://www.kraken.com"
-mkdir -p accounts/_next/static/css accounts/_next/static/chunks/pages accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E
+
+# Create the specific directory tree
+mkdir -p accounts/_next/static/css 
+mkdir -p accounts/_next/static/chunks/pages 
+mkdir -p accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E
+
+# Download CSS
 curl -L "$BASE/accounts/_next/static/css/637574239dfd98ee.css" -o "accounts/_next/static/css/637574239dfd98ee.css"
+
+# Download Logic Chunks
 CHUNKS=("polyfills-78c92fac7aa8fdd8.js" "webpack-b5405678efa2b435.js" "framework-662639a5ed894c32.js" "main-d4b0087ae4371dc0.js" "5632-863a7142674acfec.js" "4071-d43d5f730b2bf5ef.js" "5005-9cfb4316bd420e09.js" "1477-18dc1f81c4fe009f.js" "4067-cdec56fd71cd2046.js" "6960-f958fab1fa834eef.js" "8833-0f2a6386cb65a0fb.js")
-for c in "${CHUNKS[@]}"; do curl -L "$BASE/accounts/_next/static/chunks/$c" -o "accounts/_next/static/chunks/$c"; done
+for c in "${CHUNKS[@]}"; do 
+    curl -L "$BASE/accounts/_next/static/chunks/$c" -o "accounts/_next/static/chunks/$c"
+done
+
+# Download Page & Manifests
 curl -L "$BASE/accounts/_next/static/chunks/pages/_app-0ae70daee4bf5b17.js" -o "accounts/_next/static/chunks/pages/_app-0ae70daee4bf5b17.js"
 curl -L "$BASE/accounts/_next/static/chunks/pages/sign-in-8f578a596e5ea2cb.js" -o "accounts/_next/static/chunks/pages/sign-in-8f578a596e5ea2cb.js"
 curl -L "$BASE/accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E/_buildManifest.js" -o "accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E/_buildManifest.js"
 curl -L "$BASE/accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E/_ssgManifest.js" -o "accounts/_next/static/accounts-_bvWjgB7JhwiuX5g_V31E/_ssgManifest.js"
 
+# ADDITIVE LOGIC: Create a redirect so the root isn't blank
+cp index.html 404.html
